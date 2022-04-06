24baf915c5780555b2a1e65bc7e85bfa462f1688
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-04-06 17:32:26 -0300

    Requisito 7 OK

---
+++ src/components/Missions.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <h4>Missão</h4>
+      </div>
+    );
+  }
+}
+
+export default Missions;
